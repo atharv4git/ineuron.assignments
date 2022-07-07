3330fa4ad293f9e0dedbdfdddacf32be0cbc77a5
Atharv Kukarni <atharv4study@gmail.com>
2022-07-07 17:41:04 +0530

    the modified gh.sh added

--- gh.sh
+++ gh.sh
@@ -8,7 +8,7 @@ git status
 echo "enter you message:"
 read message
 echo "commiting to the repo"
-git commit -m "$message added!"
+git commit -m "$message added"
 echo "pushing to the origin"
 git push origin main
 echo "done!"
