4d3343562aa2df21ec96c93401dd1cf84b329996
Atharv Kukarni <atharv4study@gmail.com>
2022-07-07 23:26:08 +0530

    gh.bat file

---
+++ gh.bat
@@ -0,0 +1,14 @@
+@echo off
+echo "starting push batch file for repository..."
+echo "initialising a git repo if not existing"
+git init
+git status
+echo "adding dir"
+git add .
+sleep 1
+set /p message="enter message: "
+echo "commiting repo"
+git commit -m %message%
+echo "pushing commits to origin - main branch"
+git push origin main
+echo "Done!"
