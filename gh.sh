3796ce7f90cc9558d1dc27f5ad952e812a4f7bc7
Atharv Kukarni <atharv4study@gmail.com>
2022-07-07 15:01:53 +0530

    assignment 2 added

---
+++ gh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "starting bash script for pushing the changes made to the repository..."
+echo "initialising repo if not initialized"
+git init
+echo "adding dir"
+git add .
+git status
+echo "enter you message:"
+read message
+echo "commiting to the repo"
+git commit -m $message
+echo "pushing to the origin"
+git push origin main
+echo "done!"
